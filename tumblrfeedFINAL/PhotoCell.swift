44e59e9a10cb6638e410b84401cc4d771b16813f
Eric <eduong117@gmail.com>
2018-09-17 15:39:42 -0700

    first commit

---
+++ tumblrfeedFINAL/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  tumblrfeedFINAL
+//
+//  Created by Eric Duong on 9/12/18.
+//  Copyright © 2018 Eric Duong. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
